52d6f63c52bdf3c5c0c063d3590c1fd78b20c9ce
Daniel Ichiyama <danielichiyama@gmail.com>
2019-04-18 12:59:18 -1000

    problem 3

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@ fullName.innerHTML = 'Ronald McDonald';
 
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
 
+var age = document.getElementById ('age');
+age.innerHTML = '63 years old';
+
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
 
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
